0228d2c1678a38bdaeb845eb1d7dc16d649743f2
Fernando Santillan <ferdiego88@gmail.com>
2022-07-31 15:35:59 -0500

    Observables

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisesService } from '../../services/paises.service';
+import { PaisSmall } from '../../interfaces/pais.interface';
+
+import {switchMap, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-selector-page',
@@ -7,9 +12,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectorPageComponent implements OnInit {
 
-  constructor() { }
+  miFormulario: FormGroup = this.fb.group({
+    region: ['',Validators.required],
+    pais: ['',Validators.required],
+    frontera: ['',Validators.required]
+  });
+
+  regiones: string[] = [];
+  paises: PaisSmall[] = [];
+  // fronteras: string[] = [];
+  fronteras: PaisSmall[] = [];
+
+  cargando:boolean = false;
+  constructor(private fb: FormBuilder,
+              private paisesService:PaisesService) { }
 
   ngOnInit(): void {
+   this.regiones = this.paisesService.regiones;
+
+
+   //Cuando cambia la region
+  this.miFormulario.get('region')?.valueChanges
+    .pipe(
+      tap((_) => {
+        //Resetear el valor del selector pais
+        this.miFormulario.get('pais')?.reset('');
+        this.cargando = true;
+      }),
+      switchMap(region => this.paisesService.getPaisesPorRegion(region) )
+    )
+    .subscribe(paises => {
+       this.paises = paises;
+       this.cargando = false;
+    })
+
+
+//Cuando cambia el pais
+    this.miFormulario.get('pais')?.valueChanges
+      .pipe(
+        tap((_) => {
+          //Resetear el valor del selector pais
+          this.fronteras = [];
+          this.miFormulario.get('frontera')?.reset('');
+          this.cargando = true;
+        }),
+        switchMap(codigo => this.paisesService.getPaisesPorCodigo(codigo)),
+        switchMap(pais => this.paisesService.getPaisPorAlphaCode(pais?.borders!)),
+      )
+      .subscribe(paises => {
+       //this.fronteras = pais?.borders || [];
+       this.fronteras = paises;
+       this.cargando = false;
+       console.log(paises);
+
+     })
+
+  }
+
+  guardar() {
+   console.log(this.miFormulario.value);
   }
 
 }
--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,9 +1,57 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { combineLatest, Observable, of } from 'rxjs';
+import { PaisSmall, Pais } from '../interfaces/pais.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
 
-  constructor() { }
+  private _baseUrl = 'https://restcountries.com/v2';
+  private _regiones:string [] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
+
+  get regiones () {
+    return this._regiones;
+  }
+  constructor(private http: HttpClient) { }
+
+  getPaisesPorRegion(pais: String): Observable<PaisSmall[]>{
+
+    const url: string = `${this._baseUrl}/region/${pais}?fields=alpha3Code,name`;
+    return this.http.get<PaisSmall[]>(url);
+
+  }
+
+  getPaisesPorCodigo(codigo: String): Observable<Pais | null>{
+    if (!codigo) {
+      return of(null)
+    }
+    const url = `${this._baseUrl}/alpha/${codigo}`;
+
+    return this.http.get<Pais>(url);
+
+  }
+
+  getPaisName(codigo: String): Observable<PaisSmall> {
+    // https://restcountries.com/v2/alpha/per?fields=name,alpha3Code
+    const url = `${this._baseUrl}/alpha/${codigo}?fields=name,alpha3Code`;
+    return this.http.get<PaisSmall>(url);
+
+  }
+
+  getPaisPorAlphaCode(borders: string[]):Observable<PaisSmall[]> {
+
+      if (!borders) {
+        return of([])
+      }
+
+      const peticiones: Observable<PaisSmall>[] = [];
+
+      borders.forEach(alphaCode=> {
+        const peticion = this.getPaisName(alphaCode);
+        peticiones.push(peticion);
+      })
+      return combineLatest(peticiones);
+  }
 }
